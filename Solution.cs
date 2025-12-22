1ead60d9de852422705ad169d199f77a7c194300
Ayre <davidjosephibrahim@gmail.com>
2025-12-21 18:21:11 -0700

    All utility functions should now be ready

--- Solution.cs
+++ Solution.cs
@@ -1,37 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Text.Json;
-
-
 class Solution
 {
-    public class Reading(int id, int latitude, int longitude)
+    public class Reading
     {
-        int id = id;
-        int latitude = latitude;
-        int longitude = longitude;
-
-        public int Id
-        {
-            get {return id;}
-            set {id = value;}
-        }
-
-        public int Latitude
-        {
-            get {return latitude;}
-            set {latitude = value;}
-        }
-        
-        public int Longitude
-        {
-            get {return longitude;}
-            set {longitude = value;}
-        }
+        public int Id { get; set; }  
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
     }
 
 
-    public bool CompareReadings(Reading r1, Reading r2)
+    public static bool CompareReadings(Reading r1, Reading r2)
     {
         if (Math.Abs(r1.Latitude - r2.Latitude) < 0.001 && Math.Abs(r1.Longitude - r2.Longitude) < 0.001)
         {
@@ -46,7 +23,7 @@ class Solution
     * and b) an interface with a comparison function could also be used. I went with the callback since
     * it was just interesting to learn about, and not something I've used in a while.
     */
-    public Reading Search(List<Reading> readings, Func<Reading, Reading, bool> compare, Reading target)
+    public static Reading Search(List<Reading> readings, Func<Reading, Reading, bool> compare, Reading target)
     {
         if (compare == null)
         {
@@ -63,15 +40,46 @@ class Solution
         }
     }
 
-    public void JSONifyResult(string filename, Dictionary<string, int> pairs)
+    public static void JSONifyResult(string filename, Dictionary<string, int> pairs)
     {
-        // Something to handle file opening here.
-
         var entries = pairs.Select
         (
             i => string.Format("{0}: {1}", i.Key, i.Value) 
         );
-
         string finalResult = "{\n" + string.Join(",\n", entries) + "\n}";
+        File.WriteAllText(filename + ".json", finalResult);
+    }
+
+    public static void ReadCSV(string filename, List<Reading> buf)
+    {
+        string allReadings = File.ReadAllText(filename);
+        List<string> splitReadings = new(allReadings.split("\n"));
+        splitReadings.RemoveAt(0);
+
+        foreach (string readingString in splitReadings)
+        {
+            string[] fields = readingString.Split(",");
+            Reading reading = new()
+            {
+                Id = fields[0],
+                Latitude = fields[1],
+                Longitude = fields[2]
+            };
+            buf.Add(reading);
+        }
+    }
+
+    public static void ReadJSON(string filename, List<Reading> buf)
+    {
+        string allReadings = File.ReadAllText(filename);
+        allReadings = allReadings.Replace("[", "");
+        allReadings = allReadings.Replace("]", "");
+        string[] splitReadings = allReadings.Split(",");
+
+        foreach (string readingString in splitReadings)
+        {
+            Reading reading = JsonSerializer.Deserialize<Reading>(readingString)!;
+            buf.Add(reading);
+        }
     }
 }
